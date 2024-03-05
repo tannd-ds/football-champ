fbaa41d9fe0bd8f41db3092db4cfe632d9287169
Thach Le <21521421@gm.uit.edu.vn>
2024-03-05 13:23:46 +0700

    trigger

--- app/Http/Controllers/SeasonController.php
+++ app/Http/Controllers/SeasonController.php
@@ -4,25 +4,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class SeasonController extends Controller
-{ 
+{  
+    // Trang xem
     public function index()
     {
         $all_season = DB::table('season')->get();
         return response()->json($all_season);
     }
-
+    // Add be
     public function add(Request $request)
     {
         try {
-            $season = $request->input('id');
+            $season_name = $request->input('season_name');
             $start_date = $request->input('start_date');
             $end_date = $request->input('end_date');
-
+            $quantity = $request->input('quantity');
             // turn the dates into the format that the database expects
             $start_date = date('Y-m-d', strtotime($start_date));
             $end_date = date('Y-m-d', strtotime($end_date));
 
-            $data = array('id' => $season, 'start_day' => $start_date, 'end_day' => $end_date);
+            $data = array('name_season' => $season_name, 'start_day' => $start_date, 'end_day' => $end_date, 'quantity_team'=> $quantity);
             DB::table('season')->insert($data);
             return response()->json('Season added successfully');
         
@@ -30,5 +31,38 @@ class SeasonController extends Controller
             return response()->json(['error' => 'Failed to add season: ' . $e->getMessage()], 500);
         }
     }
+    // Trang chỉnh sửa
+    public function edit_season($id)
+    {
+        $edit_season = DB::table('season')->where('id', $id)->get();
+        return response()->json($edit_season);
+    }
+    // Update be
+    public function update(Request $request, $id){
+        try {
+        $season_name = $request->input('season_name');
+        $start_date = $request->input('start_date');
+        $end_date = $request->input('end_date');
+        $quantity = $request->input('quantity');
+        // turn the dates into the format that the database expects
+        $start_date = date('Y-m-d', strtotime($start_date));
+        $end_date = date('Y-m-d', strtotime($end_date));
 
+        $data = array('name_season' => $season_name, 'start_day' => $start_date, 'end_day' => $end_date, 'quantity_team'=> $quantity);
+        DB::table('season')->where('id', $id)->update($data);
+        return response()->json('Season update successfully');
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'Failed to update season: ' . $e->getMessage()], 500);
+    }
+
+    }
+    // Delete be
+    public function delete($id){
+        try{
+        DB::table('season')->where('id', $id)->delete();
+    return response()->json('Season delete successfully');
+}
+catch(\Exception $e){
+    return response()->json(['error' => 'Failed to delete season: ' . $e->getMessage()], 500);    }
+    }
 }
---
+++ footballchamp.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 05, 2024 lúc 07:22 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `footballchamp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `detailteam`
+--
+
+CREATE TABLE `detailteam` (
+  `id` int(11) NOT NULL,
+  `name_team` varchar(255) DEFAULT NULL,
+  `url_image` varchar(255) DEFAULT NULL,
+  `quantity_soccer` int(11) DEFAULT NULL,
+  `name_soccer` varchar(255) DEFAULT NULL,
+  `birth_soccer` date DEFAULT NULL,
+  `category_soccer` int(11) DEFAULT NULL,
+  `home_court` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `listteam`
+--
+
+CREATE TABLE `listteam` (
+  `season_id` int(11) DEFAULT NULL,
+  `team_id` int(11) DEFAULT NULL,
+  `day_sigin` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `result`
+--
+
+CREATE TABLE `result` (
+  `id` int(11) NOT NULL,
+  `season_id` int(11) DEFAULT NULL,
+  `team_id` int(11) DEFAULT NULL,
+  `win` int(11) DEFAULT NULL,
+  `lose` int(11) DEFAULT NULL,
+  `draw` int(11) DEFAULT NULL,
+  `total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` int(11) NOT NULL,
+  `season_id` int(11) DEFAULT NULL,
+  `day` date DEFAULT NULL,
+  `team_id_1` int(11) DEFAULT NULL,
+  `team_id_2` int(11) DEFAULT NULL,
+  `team1_score` int(11) DEFAULT NULL,
+  `team2_score` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Bẫy `schedule`
+--
+DELIMITER $$
+CREATE TRIGGER `insert_schedule` BEFORE INSERT ON `schedule` FOR EACH ROW SET NEW.team1_score = null, NEW.team2_score = null
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_schedule_result` AFTER UPDATE ON `schedule` FOR EACH ROW UPDATE result
+SET win = (SELECT COUNT(*)
+           FROM schedule
+           WHERE (team1_score > team2_score AND team_id_1 = NEW.team_id_1) OR 
+                 (team2_score > team1_score AND team_id_2 = NEW.team_id_1)),
+    lose = (SELECT COUNT(*)
+            FROM schedule
+            WHERE (team1_score < team2_score AND team_id_1 = NEW.team_id_1) OR 
+                  (team2_score < team1_score AND team_id_2 = NEW.team_id_1)),
+    draw = (SELECT COUNT(*)
+            FROM schedule
+            WHERE (team1_score = team2_score AND team_id_1 = NEW.team_id_1) OR 
+                  (team2_score = team1_score AND team_id_2 = NEW.team_id_1)),
+                  total=win*3+draw
+WHERE team_id = NEW.team_id_1
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_schedule_result2` AFTER UPDATE ON `schedule` FOR EACH ROW UPDATE result
+SET win = (SELECT COUNT(*)
+           FROM schedule
+           WHERE (team1_score > team2_score AND team_id_1 = NEW.team_id_2) OR 
+                 (team2_score > team1_score AND team_id_2 = NEW.team_id_2)),
+    lose = (SELECT COUNT(*)
+            FROM schedule
+            WHERE (team1_score < team2_score AND team_id_1 = NEW.team_id_2) OR 
+                  (team2_score < team1_score AND team_id_2 = NEW.team_id_2)),
+    draw = (SELECT COUNT(*)
+            FROM schedule
+            WHERE (team1_score = team2_score AND team_id_1 = NEW.team_id_2) OR 
+                  (team2_score = team1_score AND team_id_2 = NEW.team_id_2)),
+     total = win*3+draw
+WHERE team_id = NEW.team_id_2
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `season`
+--
+
+CREATE TABLE `season` (
+  `id` int(11) NOT NULL,
+  `name_season` varchar(255) DEFAULT NULL,
+  `start_day` date DEFAULT NULL,
+  `end_day` date DEFAULT NULL,
+  `quatity_team` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Bẫy `season`
+--
+DELIMITER $$
+CREATE TRIGGER `Delete_season_listteam` AFTER DELETE ON `season` FOR EACH ROW DELETE FROM listteam WHERE season_id = OLD.id
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Delete_season_result` AFTER DELETE ON `season` FOR EACH ROW DELETE FROM result WHERE season_id = OLD.id
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Delete_season_schedule` AFTER DELETE ON `season` FOR EACH ROW DELETE FROM schedule WHERE season_id = OLD.id
+$$
+DELIMITER ;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `detailteam`
+--
+ALTER TABLE `detailteam`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `season`
+--
+ALTER TABLE `season`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
